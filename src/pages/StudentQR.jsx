2d5a15d5ee543827429a9e1963703440d27815c3
Yoga Vigneshwar <skipnotadstudios@gmail.com>
2026-06-21 14:09:42 +0530

    fix dynamic student QR code

--- src/pages/StudentQR.jsx
+++ src/pages/StudentQR.jsx
@@ -3,6 +3,8 @@ import { QRCodeCanvas } from "qrcode.react";
 function StudentQR() {
   const student = JSON.parse(localStorage.getItem("student"));
 
+  console.log("QR STUDENT =", student);
+
   if (!student) {
     return (
       <div className="min-h-screen bg-black text-white flex items-center justify-center">
@@ -14,19 +16,26 @@ function StudentQR() {
   return (
     <div className="min-h-screen bg-black text-white flex items-center justify-center">
       <div className="bg-zinc-900 p-10 rounded-2xl text-center shadow-xl">
+        
         <h1 className="text-4xl font-bold text-yellow-400 mb-4">
           My Attendance QR 📷
         </h1>
 
-        <p className="text-2xl mb-2">{student.studentName}</p>
-        <p className="text-zinc-400 mb-6">{student.studentCode}</p>
+        <p className="text-2xl mb-2">
+          {student.studentName}
+        </p>
+
+        <p className="text-zinc-400 mb-6">
+          {student.studentCode}
+        </p>
 
         <div className="bg-white p-4 rounded-xl inline-block">
           <QRCodeCanvas
-            value={"SSF-SPD-001"}
+            value={student.studentCode || "NO-CODE"}
             size={250}
           />
         </div>
+
       </div>
     </div>
   );
